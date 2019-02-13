60995125d5c439a12f2d03bf673337d5ffbea34c
kaushik93 <kaushikprakash93@gmail.com>
2019-02-13 02:23:34 +0000

    Refactored react app basic

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-class ShoppingList extends React.Component {
-    render(){
-        return(
-            <div className="shopping-list">
-            <h1>Shoping List for{this.props.name}</h1>
-            <ul>
-                <li>Insta</li>
-                <li>Whatsapp</li>
-                <li>Facebook</li>
-            </ul>
-            </div>
-        );
-    }
-}
-ReactDOM.render(
-    <ShoppingList/>,
-    document.getElementById('root')
-  );
-  
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
